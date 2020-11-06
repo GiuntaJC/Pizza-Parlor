9c83c7e4b29bf3bdeb4ed08853b3c7d18f95743c
Joseph Giunta <jgnohack@gmail.com>
2020-11-06 14:57:39 -0800

    experiment with calculating price

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // Back-end
 function orderList() {
   this.pizzas = [];
+  this.totalPrice = 0;
   this.itemId = 0;
 }
 
@@ -9,11 +10,35 @@ orderList.prototype.assignId = function() {
   return this.currentId;
 }
 
-function pizza(crust, sauce, toppings, size) {
+orderList.prototype.addPizza = function(pie) {
+  pie.itemId = this.assignId();
+  this.pizzas.push(pie);
+}
+
+orderList.prototype.getPizzaPrice = function() {
+  const sizePrice = [["small", 8.50], ["medium", 11.50], ["large",14.50]]; // Base Price
+  const crustSizePrice = [["thin", .80], ["regular", 1], ["Deep-Dish", 2]]; // multiplier
+  const crustGlutenPrice = [[false, 1], [true, 1.3]]; // multiplier
+  const saucePrice = [["tomato", .50], ["white garlic", .75], ["alfredo", .80]]; // addition
+  const toppingsPrice = [["peperoni", .75], ["mushroom", .85], ["diced tomato", .50], ["sausage", 1], ["basil", .50], ["pineapple", .60], ["canadian bacon", .70]]; // addition
+
+  let pizzaPrice = 0;
+
+  this.pizzas.forEach(elem1) {
+    sizePrice.forEach(i, elem1) {
+      if (elem1.size == [i,0]) {
+        elem1.price = [i,1]
+      }
+    }
+  }
+}
+
+function pizza(size, crust, sauce, toppings) {
+  this.size = size;
   this.crust = crust;
   this.sauce = sauce;
   this.toppings = toppings;
-  this.size = size;
+  this.price = 0;
 }
 
 function crustType(crustSize, isGlutenFree) {
