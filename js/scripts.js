a34cc130dfef7abb0a7a68eb21c75522fc7b4d8c
Joseph Giunta <jgnohack@gmail.com>
2020-11-08 17:17:03 -0800

    add function for crust price, new test to ReadMe

--- js/scripts.js
+++ js/scripts.js
@@ -25,14 +25,28 @@ function getSizePrice(size) {
   }
 }
 
+function getCrustPrice(price, crustSize, gluten) {
+  let output = 0;
+  if(crustSize == "thin") {
+    output = price * .80;
+  } else if(crustSize == "regular") {
+    output = price;
+  } else if(crustSize == "deep-dish") {
+    output = price * 1.5;
+  }
+  if(gluten = true) {
+    output = output * 1.3;
+  }
+  return output;
+}
+
 orderList.prototype.getPizzaPrice = function() {
-  const crustSizePrice = [["thin", .80], ["regular", 1], ["Deep-Dish", 1.5]]; // multiplier
-  const crustGlutenPrice = [[false, 1], [true, 1.3]]; // multiplier
   const saucePrice = [["tomato", .50], ["white garlic", .75], ["alfredo", .80]]; // addition
   const toppingsPrice = [["peperoni", .75], ["mushroom", .85], ["diced tomato", .50], ["sausage", 1], ["basil", .50], ["pineapple", .60], ["canadian bacon", .70]]; // addition
 
   this.pizzas.forEach(function(objIndex) {
     objIndex.price = getSizePrice(objIndex.size);
+    objIndex.price = getCrustPrice(objIndex.price, objIndex.crust.crustSize, objIndex.crust.isGlutenFree);
   }); 
 }
 
